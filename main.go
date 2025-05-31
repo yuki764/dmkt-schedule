3416604ccf8c813e5a427c1b7f96a10d69a29474
Setuu <setuu@neigepluie.net>
2025-05-31 21:06:08 +0900

    change title parsing shorter

--- main.go
+++ main.go
@@ -130,6 +130,8 @@ func main() {
 				r := reForEvt.FindStringSubmatch(e)
 				if r != nil {
 					title = r[3]
+					title = strings.Replace(title, "個人配信]", "]", 1)
+					title = strings.ReplaceAll(title, "　", " ")
 
 					h, err = time.ParseDuration(r[1] + "h")
 					if err != nil {
